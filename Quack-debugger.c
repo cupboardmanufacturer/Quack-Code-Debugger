597e762de98b5932ff63538c2ca0ae5ca47239f3
cupboardmanufacturer <160159901+cupboardmanufacturer@users.noreply.github.com>
2024-02-17 19:18:35 -0800

    Update Quack-debugger v2.c

--- Quack-debugger.c
+++ Quack-debugger.c
@@ -1,24 +1,65 @@
 #include <stdio.h>
 
-void quack();
-
 int main() {
+  
+  char issue[1000];
+  char path[1000];
+  char yn;
+  
+  printf("please specify the absolute path of the file that dosent work:\n");
+  
+  scanf(" %s", path);
+  
+  if (!access(path, 0) == 0){
+      printf("file not found\n enter to continue\n");
+      
+      printf("\npress enter to continue\n");
+      while (getchar() != '\n')
+        ;
+      getchar();
+      main();
+  }
+  
+  progress();
+  
+  printf("solution has been found, fix code? (y/n)\n");
+  
 
-    char usrInput[2000]; 
-    printf("\nEnter the issue you are dealing with: ");
-    
-    while (1) { 
 
-        fgets(usrInput, sizeof(usrInput), stdin);
-        
-        quack();
-        
-        printf("Enter the issue you are dealing with: ");
-    }
+  scanf(" %c", &yn);
 
-    return 0;
+  if (yn == 'y') {
+  
+  } else if (yn == 'n') {
+  return 0;
+  } else {
+
+    printf("invalad input");
+    main();
+
+  }
+
+  
+  erase(path);
+  progress();
+  
+  printf("code fixed, thank you for using this program\n");
+  
+  return 0;
 }
 
-void quack() {
-    printf("quack\n");
-}
+int progress(){
+     int i = 0;
+     for (i = 0; i < 10000; i++){
+         printf("\rIn progress %d", i/100);
+       fflush(stdout);
+     }
+     printf("\n");
+ }
+
+int erase(char path[1000]) { 
+    FILE* FileBeingWritenTo; 
+    FileBeingWritenTo = fopen(path, "w+"); 
+    fprintf(FileBeingWritenTo, "%s", ""); 
+  fprintf(FileBeingWritenTo, "quack");
+} 
